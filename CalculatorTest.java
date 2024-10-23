5f22264206c3236b98d1b7aabd29c34b7931c98e
znolly <173570505+znolly@users.noreply.github.com>
2024-10-22 21:51:38 -0400

    testAdd before code

---
+++ CalculatorTest.java
@@ -0,0 +1,38 @@
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Write a description of CalculatorTest here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class CalculatorTest {
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    @BeforeEach
+    public void setUp() {
+    }
+
+    /**
+     * Tears down the test fixture.
+     *
+     * Called after every test case method.
+     */
+    @AfterEach
+    public void tearDown() {
+    }
+    
+    @Test
+    public void testAdd() {
+        Calculator calculator = new Calculator();
+        int result = calculator.add(2, 2);
+        assertEquals(4, result); // Validar que 2 + 2 = 4
+    }
+}
