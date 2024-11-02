d2c54c550162706a4fc6c924a99e9e31f3cfd8bf
znolly <173570505+znolly@users.noreply.github.com>
2024-11-02 13:41:30 -0400

    add Oracle Java code conventions (150003)

--- Calculator.java
+++ Calculator.java
@@ -1,40 +1,27 @@
 
 /**
- * Write a description of Calculator here.
+ * Calculator is an example project from Blé, C. (2020). 
+ *     Diseño ágil con TDD, chapter 8.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * The Calculator class provides basic arithmetic operations.
+ * It sets the argument limits for each operation provided.
+ * 
+ * @author Team_JavaVerse
+ * @version 02-11-24
  */
 public class Calculator {
     private int lowerLimit;
     private int upperLimit;
-    public Calculator(int minValue, int maxValue){
+    
+    public Calculator(int minValue, int maxValue) {
         lowerLimit = minValue;
         upperLimit = maxValue;
     }
-    
-    
-    
+        
     public int add(int arg1, int arg2) {
         validateArgs(arg1, arg2);
-        
-        if (arg1 > upperLimit) {
-            throw new ArithmeticException("First_argument_exceeds_upper_limit");
-        }
-        
-        if (arg2 < lowerLimit) {
-            throw new ArithmeticException("Second_argument_exceeds_lower_limit");
-        }
-        
-        if (arg1 < lowerLimit) {
-            throw new ArithmeticException("First_argument_exceeds_lower_limit");
-        }
-        
-        if (arg2 > upperLimit) {
-            throw new ArithmeticException("Second_argument_exceeds_upper_limit");
-        }
-        
         int result = arg1 + arg2;
+        
         if (result > upperLimit) {
             throw new ArithmeticException("Upper_limit_exceeded");
         }
@@ -43,33 +30,32 @@ public class Calculator {
     
     public int substract(int arg1, int arg2) {
         validateArgs(arg1, arg2);
-        int res = arg1 - arg2;
-        if(res<lowerLimit){
-            throw new ArithmeticException("Desbordamiento de enteros");
+        int result = arg1 - arg2;
+        
+        if (result < lowerLimit) {
+            throw new ArithmeticException("Lower_limit_exceeded");
         }
-        return res;
+        return result;
     }
     
      public boolean validateArgs(int arg1, int arg2) {
-         
         if (arg1 > upperLimit) {
-            throw new ArithmeticException("First argument exceeds upper limit");
+            throw new ArithmeticException("First_argument_exceeds_upper_limit");
         }
         
         if (arg2 < lowerLimit) {
-            throw new ArithmeticException("First argument exceeds lower limit");
+            throw new ArithmeticException("First_argument_exceeds_lower_limit");
         }
         
         if (arg1 < lowerLimit) {
-            throw new ArithmeticException("Second argument exceeds lower limit");
+            throw new ArithmeticException("Second_argument_exceeds_lower_limit");
         }
         
         if (arg2 > upperLimit) {
-            throw new ArithmeticException("Second argument exceeds upper limit");
+            throw new ArithmeticException("Second_argument_exceeds_upper_limit");
         }
         return true;
     }
-    
     
     public void setUpperLimit(int limit) {
         upperLimit = limit;
@@ -86,7 +72,4 @@ public class Calculator {
     public int getLowerLimit() {
         return lowerLimit;
     }
-    
-    
-    
 }
--- CalculatorTest.java
+++ CalculatorTest.java
@@ -2,81 +2,80 @@
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
- * Write a description of CalculatorTest here.
+ * Calculator is an example project from Blé, C. (2020). 
+ *     Diseño ágil con TDD, chapter 8.
+ *     
+ * The CalculatorTest class contains unit tests for the Calculator class.
+ * It uses the JUnit 5 testing framework to verify the correctness 
+ *     of the arithmetic operations and its limits.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Team_JavaVerse
+ * @version 02-11-24
  */
 public class CalculatorTest {
-    /**
-     * Sets up the test fixture.
-     *
-     * Called before every test case method.
-     */
     Calculator calculator;
+    
     @BeforeEach
     public void setUp() {
         calculator = new Calculator(-100, 100);
     }
 
-    /**
-     * Tears down the test fixture.
-     *
-     * Called after every test case method.
-     */
     @AfterEach
     public void tearDown() {
     }
     
     @Test
-    public void test2add2equals4() { //Add
+    public void test2add2equals4() { // Add
         int result = calculator.add(2, 2);
         assertEquals(4, result);
     }
     
     @Test
-    public void test2add5equals7() { //AddWithDifferentArguments
+    public void test2add5equals7() { // AddWithDifferentArguments
         int result = calculator.add(2, 5);
         assertEquals(7, result);
     }
     
     @Test
-    public void testSubstract5And3Is2() { //Substract
+    public void testSubstract5And3Is2() { // Substract
         int result = calculator.substract(5, 3);
         assertEquals(2, result);
     }
     
     @Test
-    public void testSubstract3And5IsNeg2() { //SubstractReturningNegative
+    public void testSubstract3And5IsNeg2() { // SubstractReturningNegative
         int result = calculator.substract(3, 5);
         assertEquals(-2, result);
     }
     
     @Test
-     public void testSubstractSettingLimitValues(){ //SubstractSettingLimitValues
-       int result = calculator.substract(5, 10);
-       assertEquals(-5, result);
+     public void testSubstractSettingLimitValues(){ // SubstractSettingLimitValues
+        int result = calculator.substract(5, 10);
+        assertEquals(-5, result);
     }
     
     @Test
-    public void testSubstractExcedingLowerLimit(){ //SubstractExcedingLowerLimit
+    public void testSubstractExcedingLowerLimit(){ // SubstractExcedingLowerLimit
         Calculator calculator = new Calculator(-100, 100);
-        try{
+        
+        try {
             int result = calculator.substract(10, 150);
             fail("Exception is not being thrown when exceeding lower limit");
-        }catch(ArithmeticException e){
+        } catch (ArithmeticException e) {
             // Ok, the SUT works as expected
         }
     }
+    
     @Test 
     public void testSetUpperLimit() {
         calculator.setUpperLimit(10);
         int result = calculator.getUpperLimit();
         assertEquals(10, result);
     }
+    
     @Test
     public void testSetAndGetLimit() {
         calculator.setUpperLimit(10);
@@ -84,13 +83,15 @@ public class CalculatorTest {
         assertEquals(10, result);
         assertEquals(10, calculator.getUpperLimit());
     }
+    
     @Test
-    public void addExceedingUpperLimit(){ //SubstractExcedingLowerLimit
+    public void addExceedingUpperLimit(){ // SubstractExcedingLowerLimit
         Calculator calculator = new Calculator(-100, 100);
-        try{
+        
+        try {
             int result = calculator.add(10, 150);
             fail("This_should_fail_vía_if_exceeding_upper_limit");
-        }catch(ArithmeticException e){
+        } catch (ArithmeticException e){
             // Ok, the SUT works as expected
         }
     }
@@ -98,6 +99,7 @@ public class CalculatorTest {
     @Test
     public void ExceedArguments() {
         Calculator calculator = new Calculator(-100, 100);
+        
         try {
             calculator.add(calculator.getUpperLimit() + 1, calculator.getLowerLimit() - 1);
             fail("THIS_SHOULD_FAIL:_ARGUMENTS_THAT_EXCEED_LIMITS");
@@ -109,6 +111,7 @@ public class CalculatorTest {
     @Test
     public void ExceedArgumentsInverse() {
         Calculator calculator = new Calculator(-100, 100);
+        
         try {
             calculator.add(calculator.getLowerLimit() - 1, calculator.getUpperLimit() + 1);
             fail("THIS_SHOULD_FAIL_:ARGUMENTS_EXCEEDS_LIMITS");
@@ -120,6 +123,7 @@ public class CalculatorTest {
     @Test
     public void testArgumentsExceedLimitsOnSubtract() {
         Calculator calculator = new Calculator(-100, 100);
+        
         try {
             calculator.substract(calculator.getUpperLimit() + 1, calculator.getLowerLimit() - 1);
             fail("This should fail: arguments exceed limits");
@@ -131,6 +135,7 @@ public class CalculatorTest {
     @Test
     public void testArgumentsValidateExceedLimitsInverse() {
         Calculator calculator = new Calculator(-100, 100);
+        
         try {
             calculator.validateArgs(calculator.getLowerLimit() - 1, calculator.getUpperLimit() + 1);
             fail("This should fail: arguments exceed limits");
@@ -138,5 +143,4 @@ public class CalculatorTest {
             // La excepción se lanzó correctamente, prueba aprobada
         }
     }
-
 }
